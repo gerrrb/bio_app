81a1d180bbe9a472d265b6fec45eff0d95f16027
gerrrb <98877925+gerrrb@users.noreply.github.com>
2022-03-07 17:41:52 +0700

    Update page_pengantar.py

--- page_pengantar.py
+++ page_pengantar.py
@@ -6,7 +6,7 @@ def show_page():
     for i in range(8):
         st.sidebar.write("")
 
-    intro_markdown = read_markdown("docs/markdown/introduction.md")
+    intro_markdown = read_markdown("introduction.md")
     st.markdown(intro_markdown)
 
     return
